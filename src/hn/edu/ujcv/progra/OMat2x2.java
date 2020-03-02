8b4b014b22e0070ed45b47e1237513598af4cd67
Ariel <arielt2013@hotmail.com>
2020-03-02 07:41:11 -0600

    Agregando Constructor y Metodo Rot

--- src/hn/edu/ujcv/progra/OMat2x2.java
+++ src/hn/edu/ujcv/progra/OMat2x2.java
@@ -1,66 +1,93 @@
 package hn.edu.ujcv.progra;
 
-public class OMat2x2 {
+public class OMat2x2
+{
+    // MIEMBROS O ATRIBUTOS
 
-    // miembros
-
-
-    // accesoras y mutadoras
-    //getFilaX
-    //getColY
-
-    // metodos
-
-    public OMat2x2 transpuesta(){
-        //TODO: implementar
-        return new OMat2x2();
-    }
-
-    public OMat2x2 inversa(){
-        //TODO: implementar
-        return new OMat2x2();
-    }
-    public OMat2x2 suma(OVecR2 b){
-        //TODO: implementar
-        return new OMat2x2();
-    }
-
-    public OMat2x2 resta(OVecR2 b){
-        //TODO: implementar
-        return new OMat2x2();
-    }
-
-    public OMat2x2 mult(OVecR2 b){
-        //TODO: implementar
-        return new OMat2x2();
-    }
-
-    public double determinante(){
-        //TODO: implementar
-        return 0.0f;
-    }
-
-    // Metodos de la clase.
-    public static OMat2x2 rot(double alpha){
-        //TODO: implementar
-        return new OMat2x2();
-    }
-
-    public static OMat2x2 identidad(){
-        //TODO: implementar
-        return new OMat2x2();
-    }
+    OVecR2 col1;
+    OVecR2 col2;
 
 
-    // constructores
+    // CONSTRUCTORES
+
     public OMat2x2(){}
 
     public OMat2x2(OVecR2 a){}
 
     public OMat2x2(double m11, double m12,
-                   double m21, double m22){}
+                   double m21, double m22)
+    {
+        this.col1 = new OVecR2(m11, m21);
+        this.col2 = new OVecR2(m12, m22);
+    }
 
     public OMat2x2(OVecR2 a, OVecR2 b, boolean esColumna){}
 
 
+    // ACCESORAS Y MUTADORAS
+
+    //getFilaX
+
+    //getColY
+
+
+    // METODOS DE LA CLASE
+
+    public OMat2x2 transpuesta()
+    {
+        //TODO: implementar
+        return new OMat2x2();
+    }
+
+    public OMat2x2 inversa()
+    {
+        //TODO: implementar
+        return new OMat2x2();
+    }
+
+    public OMat2x2 suma(OVecR2 b)
+    {
+        //TODO: implementar
+        return new OMat2x2();
+    }
+
+    public OMat2x2 resta(OVecR2 b)
+    {
+        //TODO: implementar
+        return new OMat2x2();
+    }
+
+    public OMat2x2 mult(OVecR2 b)
+    {
+        //TODO: implementar
+        return new OMat2x2();
+    }
+
+    public double determinante()
+    {
+        //TODO: implementar
+        return 0.0f;
+    }
+
+
+    // METODOS DE LA CLASE
+    public static OMat2x2 rot(double alpha)
+    {
+        // cos a   -sin a
+        // sin a   cos a
+
+        double m11, m12, m21, m22;
+
+        m11 = Math.cos(alpha);
+        m12 = -Math.sin(alpha);
+        m21 = Math.sin(alpha);
+        m22 = Math.cos(alpha);
+
+        return new OMat2x2(m11, m12, m21, m22);
+    }
+
+    public static void identidad()
+    {
+        //TODO: implementar
+    }
 }
