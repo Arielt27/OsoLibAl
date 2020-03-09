7b49709050b99bc3d50aa7c2af94a00f640de604
Ariel <arielt2013@hotmail.com>
2020-03-08 20:17:01 -0600

    Implementación de Constructor, Metodos Get y Set y de Metodos de Suma y Resta de Vectores en R4

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import hn.edu.ujcv.progra.OVecR2;
 import hn.edu.ujcv.progra.OVecR3;
+import hn.edu.ujcv.progra.OVecR4;
 
 public class Main
 {
@@ -13,6 +14,8 @@ public class Main
 
         OVecR2 V2 = new OVecR2(4,3);
 
+        OVecR2 V3 = new OVecR2(3,8);
+
           // Vectores en R3
 
         OVecR3 Ve1 = new OVecR3(2,6,3);
@@ -20,6 +23,15 @@ public class Main
         OVecR3 Ve2 = new OVecR3(1,3,4);
 
         OVecR3 Ve3 = new OVecR3(5,4,7);
+
+          // Vectores en R4
+
+        OVecR4 Vec1 = new OVecR4(5,7,6,3);
+
+        OVecR4 Vec2 = new OVecR4(1,2,3,4);
+
+        OVecR4 Vec3 = new OVecR4(9,1,0,6);
+
 
         // PROGRAMA PRINCIPAL
 
@@ -31,16 +43,19 @@ public class Main
         System.out.println("VECTORES EN R2 ");
         System.out.println(" ");
 
-        System.out.println("El Vector 1 es: " + V1);                    // Mostrando Objeto Vector llamado V1
+        System.out.println("El Vector 1 es: " + V1);                       // Mostrando Objeto Vector llamado V1
         System.out.println(" ");
 
-        System.out.println("El Vector 2 es: " + V2);                    // Mostrando Objeto Vector llamado V2
+        System.out.println("El Vector 2 es: " + V2);                       // Mostrando Objeto Vector llamado V2
         System.out.println(" ");
 
-        System.out.println("Suma de Vectores en R2 " + V2.suma(V1));     // Aplicando Metodo Suma de Clase OvecR2
+        System.out.println("El Vector 3 es: " + V3);                       // Mostrando Objeto Vector llamado V3
         System.out.println(" ");
 
-        System.out.println("Resta de Vectores en R2 " + V1.resta(V2));   // Aplicando Metodo Resta de Clase OvecR2
+        System.out.println("Suma de Vectores en R2 " + V2.suma(V1));       // Aplicando Metodo Suma de Clase OvecR2
+        System.out.println(" ");
+
+        System.out.println("Resta de Vectores en R2 " + V3.resta(V2));     // Aplicando Metodo Resta de Clase OvecR2
         System.out.println(" ");
 
           // Vectores en R3
@@ -48,19 +63,40 @@ public class Main
         System.out.println("VECTORES EN R3 ");
         System.out.println(" ");
 
-        System.out.println("El Vector 1 es: " + Ve1);                   // Mostrando Objeto Vector llamado Ve1
+        System.out.println("El Vector 1 es: " + Ve1);                      // Mostrando Objeto Vector llamado Ve1
         System.out.println(" ");
 
-        System.out.println("El Vector 2 es: " + Ve2);                   // Mostrando Objeto Vector llamado Ve2
+        System.out.println("El Vector 2 es: " + Ve2);                      // Mostrando Objeto Vector llamado Ve2
         System.out.println(" ");
 
-        System.out.println("El Vector 3 es: " + Ve3);                   // Mostrando Objeto Vector llamado Ve3
+        System.out.println("El Vector 3 es: " + Ve3);                      // Mostrando Objeto Vector llamado Ve3
         System.out.println(" ");
 
-        System.out.println("Suma de Vectores en R3 " + Ve2.suma(Ve1));   // Aplicando Metodo Suma de Clase OvecR3
+        System.out.println("Suma de Vectores en R3 " + Ve2.suma(Ve1));     // Aplicando Metodo Suma de Clase OvecR3
         System.out.println(" ");
 
-        System.out.println("Resta de Vectores en R3 " + Ve1.resta(Ve3)); // Aplicando Metodo Resta de Clase OvecR3
+        System.out.println("Resta de Vectores en R3 " + Ve1.resta(Ve3));   // Aplicando Metodo Resta de Clase OvecR3
         System.out.println(" ");
+
+          // VECTORES EN R4
+
+        System.out.println("VECTORES EN R4 ");
+        System.out.println(" ");
+
+        System.out.println("El Vector 1 es: " + Vec1);                     // Mostrando Objeto Vector llamado Vec1
+        System.out.println(" ");
+
+        System.out.println("El Vector 2 es: " + Vec2);                     // Mostrando Objeto Vector llamado Vec2
+        System.out.println(" ");
+
+        System.out.println("El Vector 3 es: " + Vec3);                     // Mostrando Objeto Vector llamado Vec3
+        System.out.println(" ");
+
+        System.out.println("Suma de Vectores en R4 " + Vec1.suma(Vec2));   // Aplicando Metodo Suma de Clase OvecR4
+        System.out.println(" ");
+
+        System.out.println("Resta de Vectores en R4 " + Vec2.resta(Vec3)); // Aplicando Metodo Resta de Clase OvecR4
+        System.out.println(" ");
+
     }
 }
--- src/hn/edu/ujcv/progra/OVecR4.java
+++ src/hn/edu/ujcv/progra/OVecR4.java
@@ -2,11 +2,93 @@ package hn.edu.ujcv.progra;
 
 public class OVecR4
 {
-// MIEMBROS O ATRIBUTOS
+    // MIEMBROS O ATRIBUTOS
+
+    private double x;
+    private double y;
+    private double z;
+    private double t;
+
 
     // CONSTRUCTORES
 
+    public OVecR4(double x, double y, double z, double t)
+    {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.t = t;
+    }
+
+
     // ACCESORAS Y MUTADORAS
 
+      // Get y Set Para x
+
+    public double getX()
+    {
+        return x;
+    }
+
+    public void setX(double x)
+    {
+        this.x = x;
+    }
+
+      // Get y Set Para y
+
+    public double getY()
+    {
+        return y;
+    }
+
+    public void setY(double y)
+    {
+        this.y = y;
+    }
+
+      // Get y Set Para z
+
+    public double getZ()
+    {
+        return z;
+    }
+
+    public void setZ(double z)
+    {
+        this.z = z;
+    }
+
+      // Get y Set Para t
+
+    public double getT()
+    {
+        return t;
+    }
+
+    public void setT(double t)
+    {
+        this.t = t;
+    }
+
+
     // METODOS
+
+    public OVecR4 suma(OVecR4 b)
+    {
+        return new OVecR4(this.x + b.x, this.y + b.y, this.z + b.z, this.t + b.t);
+    }
+
+    public OVecR4 resta(OVecR4 b)
+    {
+        return new OVecR4(this.x - b.x, this.y - b.y, this.z - b.z, this.t + b.t);
+    }
+
+
+    // METODO TO STRING
+    @Override
+    public String toString()
+    {
+        return " i: " + getX() + " j: " + getY() + " k: " + getZ() + " t: " + getT();
+    }
 }
