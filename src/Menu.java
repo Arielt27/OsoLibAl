8ecff6323d10ffa659c98f0c2f206da8295926c9
Ariel <arielt2013@hotmail.com>
2020-03-22 12:53:03 -0600

    Implementada Clase Menu para mostrar menus del programa

---
+++ src/Menu.java
@@ -0,0 +1,146 @@
+public class Menu
+{
+    // METODOS PARA MENUS
+
+    public String menuPrincipal()
+    {
+        System.out.println("******LIBRERIA DE ALGEBRA LINEAL******");
+        System.out.println("1- Vectores");
+        System.out.println("2- Matrices");
+        System.out.println("3- Salir");
+
+        String A = "Su elección es: ";
+
+        return A;
+    }
+
+    public String Vectores()
+    {
+        System.out.println(" ");
+        System.out.println("******VECTORES******");
+        System.out.println("1- Vectores R2");
+        System.out.println("2- Vectores R3");
+        System.out.println("3- Vectores R4");
+
+        String A = "Su elección es: ";
+
+        return A;
+    }
+
+    public String VectoresR2()
+    {
+        System.out.println(" ");
+        System.out.println("*****VECTORES EN R2*****");
+        System.out.println("1- Suma ");
+        System.out.println("2- Resta");
+        System.out.println("3- Producto Punto");
+        System.out.println("4- Magnitud");
+
+        String A = "Su elección es: ";
+
+        return A;
+    }
+
+    public String VectoresR3()
+    {
+        System.out.println(" ");
+        System.out.println("*****VECTORES EN R3*****");
+        System.out.println("1- Suma ");
+        System.out.println("2- Resta");
+        System.out.println("3- Producto Cruz");
+        System.out.println("4- Producto Punto");
+        System.out.println("5- Magnitud");
+
+        String A = "Su elección es: ";
+
+        return A;
+    }
+
+    public String VectoresR4()
+    {
+        System.out.println(" ");
+        System.out.println("*****VECTORES EN R4*****");
+        System.out.println("1- Suma ");
+        System.out.println("2- Resta");
+        System.out.println("3- Producto Punto");
+        System.out.println("4- Magnitud");
+        System.out.println("5- Quaternion Suma");
+
+        String A = "Su elección es: ";
+
+        return A;
+    }
+
+    public String Matrices()
+    {
+        System.out.println(" ");
+        System.out.println("******MATRICES******");
+        System.out.println("1- Matrices 2x2");
+        System.out.println("2- Matrices 3x3");
+        System.out.println("3- Matrices 4x4");
+
+        String A = "Su elección es: ";
+
+        return A;
+    }
+
+    public String Matrices2x2()
+    {
+        System.out.println(" ");
+        System.out.println("******MATRICES 2X2******");
+        System.out.println("1- Suma");
+        System.out.println("2- Resta");
+        System.out.println("3- Multiplicación");
+        System.out.println("4- Determinante");
+        System.out.println("5- Transpuesta");
+        System.out.println("6- Inversa");
+        System.out.println("7- Identidad");
+        System.out.println("8- Rotación");
+        System.out.println("9- Matriz por Vector");
+
+        String A = "Su elección es: ";
+
+        return A;
+    }
+
+    public String Matrices3x3()
+    {
+        System.out.println(" ");
+        System.out.println("******MATRICES 3X3******");
+        System.out.println("1- Suma");
+        System.out.println("2- Resta");
+        System.out.println("3- Multiplicación");
+        System.out.println("4- Determinante");
+        System.out.println("5- Transpuesta");
+        System.out.println("6- Inversa");
+        System.out.println("7- Identidad");
+        System.out.println("8- Rotacion X");
+        System.out.println("9- Rotacion Y");
+        System.out.println("10 Rotacion Z");
+
+        String A = "Su elección es: ";
+
+        return A;
+    }
+
+    public String Matrices4x4()
+    {
+        System.out.println(" ");
+        System.out.println("******MATRICES 4X4******");
+        System.out.println("1- Suma");
+        System.out.println("2- Resta");
+        System.out.println("3- Multiplicación");
+        System.out.println("4- Determinante");
+        System.out.println("5- Transpuesta");
+        System.out.println("6- Inversa");
+        System.out.println("7- Identidad");
+        System.out.println("8- Rotacion X");
+        System.out.println("9- Rotacion Y");
+        System.out.println("10 Rotacion Z");
+        System.out.println("11 Rotacion T");
+
+        String A = "Su elección es: ";
+
+        return A;
+    }
+}
